9c524b6a626214eb0fc61135d77ca6bb3ab3a2e0
deepankarbhowmick <41232042+deepankarbhowmick@users.noreply.github.com>
2022-06-17 14:50:33 +0200

    Create CompoundEmployeeException.groovy

---
+++ com/compoundemployee/successfactors/CompoundEmployeeException.groovy
@@ -0,0 +1,8 @@
+package com.compoundemployee.successfactors
+
+class CompoundEmployeeException extends Exception {
+
+    CompoundEmployeeException(String message) {
+        super(message)
+    }
+}
